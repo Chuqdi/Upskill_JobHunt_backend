63688b53275e85eacd07052b5f09d57976d68aea
cyba <morganhezekiah59@gmail.com>
2022-03-10 10:19:05 +0100

    adding favicon ink

--- jh/settings.py
+++ jh/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
