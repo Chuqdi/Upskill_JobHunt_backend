8a12434f9a1c2108abcf89f0729211857ee9603d
cyba <morganhezekiah59@gmail.com>
2022-04-08 01:31:41 +0100

    recent now

--- utils/Frontend.py
+++ utils/Frontend.py
@@ -1,3 +1,5 @@
-FRONTEND_DOMAIN ="http://localhost:3000/"
+# FRONTEND_DOMAIN ="http://localhost:3000/"
 
 # FRONTEND_DOMAIN ="https://owerrijobhuntstagingenviron.netlify.app/"
+
+FRONTEND_DOMAIN ="http://owerrijobhunt.ng/"
